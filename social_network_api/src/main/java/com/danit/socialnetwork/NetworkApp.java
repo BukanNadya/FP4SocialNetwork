715e2e0ce35e1433d93af6835500d3249a5870f9
AndriiHorbenko <118114091+AndriiHorbenko@users.noreply.github.com>
2023-04-20 16:44:15 +0300

    [US12] Add connection to the H2 db and create user table. (#10)
    * Entity User created and connection to H2db
    
    * Update datatype format from Date to LocalDate
    
    * Update datatype format from Date to LocalDate
    
    * Change from app to com.danit.socialnetwork
    
    ---------
    
    Co-authored-by: AlexKhmarenko <106476677+AlexKhmarenko@users.noreply.github.com>

--- social_network_api/src/main/java/com/danit/socialnetwork/NetworkApp.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/NetworkApp.java
@@ -1,4 +1,4 @@
-package app;
+package com.danit.socialnetwork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ social_network_api/src/main/java/com/danit/socialnetwork/model/User.java
@@ -0,0 +1,41 @@
+package com.danit.socialnetwork.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Entity(name = "users")
+@Data
+public class User {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private Integer userId;
+
+  @Column(name = "username")
+  private String username;
+
+  @Column(name = "password")
+  private String password;
+
+  @Column(name = "email")
+  private String email;
+
+  @Column(name = "created_date")
+  private LocalDate createdDate;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "profile_background_image_url")
+  private String profileBackgroundImageUrl;
+
+  @Column(name = "profile_image_url")
+  private String profileImageUrl;
+
+}
