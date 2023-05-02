a46a253bbd4ff034a23c088cd1585246ce377a05
AndriiHorbenko <118114091+AndriiHorbenko@users.noreply.github.com>
2023-05-02 12:55:52 +0300

    US17 Setup CORS policy (#19)
    * Setup CORS policy
    
    * Correction and improvements with changes to pom.xml
    
    * Correction and improvements with changes to pom.xml
    
    * Updated Config class with CORS
    
    * Update once more

--- social_network_api/src/main/java/com/danit/socialnetwork/config/JavaMailSenderConfig.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/config/JavaMailSenderConfig.java
@@ -6,7 +6,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 @Configuration
@@ -43,5 +52,6 @@ public class JavaMailSenderConfig {
 
     return mailSender;
   }
+
 
 }
--- social_network_api/src/main/java/com/danit/socialnetwork/security/WebSecurityConfig.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/security/WebSecurityConfig.java
@@ -6,13 +6,21 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -35,29 +43,43 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http
-        .csrf().disable()
-        .headers().frameOptions().disable();
+    http.csrf().disable().headers().frameOptions().disable();
 
-    http
-        .authorizeRequests().antMatchers("/login").permitAll()
-        .antMatchers("/h2/**",
-            "/sendLetter", "/activate",
-            "/checkUsername", "/registration", "/").permitAll()
-        .antMatchers("/home").authenticated()
-        .anyRequest().authenticated();
+    http.authorizeRequests().antMatchers("/login").permitAll()
+        .antMatchers("/h2/**", "/sendLetter", "/activate", "/checkUsername", "/registration", "/").permitAll()
+        .antMatchers("/home")
+        .authenticated()
+        .anyRequest()
+        .authenticated();
 
     http.rememberMe();
 
-    http
-        .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint);
-    http
-        .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    http.exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint);
+    http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
     http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 
-    http.logout()
-        .logoutRequestMatcher(new AntPathRequestMatcher("/"));
+    http.logout().logoutRequestMatcher(new AntPathRequestMatcher("/"));
   }
+
+  // CORS filter
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http.cors(Customizer.withDefaults()) // by default uses a Bean by the name of corsConfigurationSource
+        .authorizeRequests(auth -> auth.anyRequest().authenticated()).httpBasic(Customizer.withDefaults()).build();
+  }
+
+  // CORS configuration
+  @Bean
+  CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT"));
+    configuration.setAllowedHeaders(List.of("Authorization"));
+    configuration.setMaxAge(3600L);
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
+
 }
