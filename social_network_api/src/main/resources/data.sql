83aadf1874ee774e84b6769c44b44b641206531c
AlexKhmarenko <106476677+AlexKhmarenko@users.noreply.github.com>
2023-05-11 00:08:35 +0300

    [US33] Connect to external DataBase (PostgreSQL) (#147)

--- social_network_api/src/main/resources/data.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM USERS;
-
-INSERT INTO USERS VALUES (1,'2023-03-27 15:32:42.000000', '1978-07-20','andrew@gmail.com', 'andrii', '1234', '/headerPhoto/railway1.png','/photo/avatar.png', 'Andrii');
-INSERT INTO USERS VALUES (2,'2023-04-27 12:32:42.000000', '1999-09-30','dilan@gmail.com', 'dilan', '1234', '/headerPhoto/railway2.png','/photo/spiderman.png', 'Dilan');
-INSERT INTO USERS VALUES (3,'2023-04-29 12:32:42.000000', '1993-02-25','bob@gmail.com', 'bob', '1234', null,null, 'Bob');
-INSERT INTO USERS VALUES (4,'2023-04-29 12:32:42.000000', '1993-07-15','chris@gmail.com', 'chris', '1234', '/headerPhoto/railway21.png','/photo/spiderman1.png', 'Chris');
-INSERT INTO USERS VALUES (5,'2023-04-29 12:32:42.000000', '1980-06-05','khmarenko.a@gmail.com', 'alex', '$2a$10$zE/4Av3BU82nLpolkjT0wuAY9PpmQDN.9lUQZujnbk5ai6tqReRQK', '/headerPhoto/railway21.png','/photo/spiderman1.png', 'Alex');
