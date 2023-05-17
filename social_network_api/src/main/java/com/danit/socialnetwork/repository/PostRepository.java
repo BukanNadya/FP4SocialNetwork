1cf3f831bd3a0ac2b1c5e823555d36e456b7aa8c
AndriiHorbenko <118114091+AndriiHorbenko@users.noreply.github.com>
2023-05-17 15:42:22 +0300

    Us22.2 Add to PostRepo to return user own posts (#162)
    * Correction and improvements with changes to pom.xml
    
    * [US22.2] Update PostRepository return user posts by id
    
    * [US22.2] Update PostRepository return user posts by id pom.xml

--- social_network_api/src/main/java/com/danit/socialnetwork/repository/PostRepository.java
+++ social_network_api/src/main/java/com/danit/socialnetwork/repository/PostRepository.java
@@ -11,7 +11,7 @@ public interface PostRepository extends JpaRepository<Post, Integer> {
 
   @Query(nativeQuery = true, value = "SELECT * FROM POSTS "
       + "LEFT JOIN USER_FOLLOWS ON POSTS.USER_ID = USER_FOLLOWS.USER_FOLLOWING_ID "
-      + "where USER_FOLLOWS.USER_FOLLOWER_ID = :userId order by POSTS.SENT_DATETIME DESC")
+      + "where USER_FOLLOWS.USER_FOLLOWER_ID = :userId or POSTS.USER_ID = :userId order by POSTS.SENT_DATETIME DESC")
   List<Post> findAllPostsFromToFollow(Integer userId, Pageable pageable);
 
 }
